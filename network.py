4195fa85cc10a451435ff6ce6d2dc6493322e78e
Jeet Thakkar <jeet.thakkar@msisurfaces.com>
2022-09-27 18:53:56 -0700

    added trace messages into ClientNetwork

--- network.py
+++ network.py
@@ -46,7 +46,7 @@ class ServerNetwork:
     @staticmethod
     def server_parse_mesg(source_ip: str, source_port: int, message: json):
         try:
-            message_dict = json.load(message.decode())
+            message_dict = json.loads(message.decode())
             user_request = message_dict['request']
 
             if user_request == "register":
@@ -121,6 +121,7 @@ class ClientNetwork:
     follower_list: [str]
 
     def __init__(self, host='127.0.0.1', port_tracker=5001, port_peer_left=5002, port_peer_right=5003):
+        print("Initializing the client")
         self.socket_tracker = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket_peer_left = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket_peer_right = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -128,38 +129,51 @@ class ClientNetwork:
         self.port_tracker = port_tracker
         self.port_peer_left = port_peer_left
         self.port_peer_right = port_peer_right
+        print(f"Client has been initialized with the IP={self.host}, and ports=[{self.port_tracker},"
+              f"{self.port_peer_left},{self.port_peer_right} ]")
 
     def setup(self):
+        print(f"trying to bind TRACKER_SOCKET to  the socket@{self.host}:{self.port_tracker}")
         try:
             self.socket_tracker.bind((self.host, self.port_tracker))
         except OSError:
             print(f"port={self.port_tracker} for tracker comms is being used by another process. "
                   f"please try another port.")
             exit(1)
+        print("bind successful!")
+        print(f"trying to bind LEFT_PEER_SOCKET to the socket@{self.host}:{self.port_peer_left}")
         try:
             self.socket_peer_left.bind((self.host, self.port_peer_left))
         except OSError:
             print(f"port={self.port_peer_left} for peer_LEFT comms is being used by another process. "
                   f"please try another port.")
             exit(1)
+        print("bind successful!")
+        print(f"trying to bind RIGHT_PEER_SOCKET to the socket@{self.host}:{self.port_peer_right}")
         try:
             self.socket_peer_right.bind((self.host, self.port_peer_right))
         except OSError:
             print(f"port={self.port_peer_left} for peer_RIGHT comms is being used by another process. "
                   f"please try another port.")
             exit(1)
+        print("bind successful!")
 
     def client_register(self, handle: str):
+        print("Compiling the REGISTER REQUEST")
         dict_message = {'request': 'register', 'handle': handle, 'source_ip': self.host,
-                        'port_tracker': self.port_tracker, 'port_peer_left': self.port_peer_left,
-                        'port_peer_right': self.port_peer_right}
-
+                        'tracker_port': self.port_tracker, 'peer_port_left': self.port_peer_left,
+                        'peer_port_right': self.port_peer_right}
+        print(f"REGISTER_REQUEST=> {dict_message}")
+        binary_request_message = json.dumps(dict_message).encode()
+        print(f"REGISTER_REQUEST_BINARY => {binary_request_message}")
         raw_message = None
         while True:
-            self.socket_tracker.sendto(json.dumps(dict_message).encode(), (TRACKER_URL, TRACKER_PORT))
+            self.socket_tracker.sendto(binary_request_message, (TRACKER_URL, TRACKER_PORT))
+            print(f"sent REGISTER_REQUEST_BINARY to {TRACKER_URL}:{TRACKER_PORT}")
             self.socket_tracker.settimeout(30)
             try:
                 raw_message = self.socket_tracker.recvfrom(1024)
+                print(f"Received RAW_MESSAGE={raw_message}")
             except TimeoutError:
                 print("the previous message to the tracker did not get a response. will try again")
                 continue
